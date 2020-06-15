67056bad593d32338ff2d1a7cdea26394743cf0b
Ace Alrich <hex0@live.com>
2020-06-15 07:56:02 +0100

    find name

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 var fs = require("fs");
+var express = require("express");
 var files = fs.readdirSync("/Code/Extract-Image-PDF");
 
-const http = require("http");
-
-const hostname = "127.0.0.1";
-const port = 5500;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end(JSON.stringify(files));
+var app = express();
+const port = 5501;
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/list`);
+app.get("/", function (req, res) {
+  res.json(files);
 });
+app.listen(port, () => console.log(port));
+
+// const server = http.createServer((req, res, next) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.end();
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}`);
+// });
--- myScripts.js
+++ myScripts.js
@@ -12,5 +12,13 @@ searchInput.addEventListener("keydown", (event) => {
   }
 });
 
-let promise = fetch("http://127.0.0.1:5500/list");
-promise.then((a) => console.log(a.body.JSON));
+const getFileList = async () => {
+  const response = await fetch("http://127.0.0.1:5501/");
+  const data = await response.json();
+  console.log(data);
+};
+getFileList();
+
+const searchPrtialMatch = () => {
+  searchInput.value;
+};
